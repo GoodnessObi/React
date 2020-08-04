85518aa1ba959d00461189547b5f9a38e5bdc8df
goodness obi <goodnessobi@gmail.com>
2020-08-04 22:49:36 +0100

    search for user based on search form- padding props up

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 class App extends Component {
@@ -11,23 +12,27 @@ class App extends Component {
 		loading: false,
 	};
 
-	async componentDidMount() {
+	static PropTypes = {
+		searchUsers: PropTypes.func.isRequired,
+	};
+
+	//search github users
+	searchUsers = async (text) => {
 		this.setState({ loading: true });
 
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
 
-		this.setState({ users: res.data, loading: false });
-		// console.log(res.data);
-	}
+		this.setState({ users: res.data.items, loading: false });
+	};
 
 	render() {
 		return (
 			<div className='App'>
 				<Navbar></Navbar>
 				<div className='container'>
-					<Search />
+					<Search searchUsers={this.searchUsers} />
 					<Users loading={this.state.loading} users={this.state.users}></Users>
 				</div>
 			</div>
