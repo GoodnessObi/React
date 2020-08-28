c3c419fa89e25977d838e102cdd917a40ff7e8c2
GoodnessObi <goodnessobi@gmail.com>
2020-08-28 21:02:37 +0100

    complete todo-app - add new todo item to list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component {
 	state = {
@@ -8,7 +9,7 @@ class App extends Component {
 			{ id: 2, content: 'walk dog' },
 		],
 	};
-	delteteTodo = (id) => {
+	deleteTodo = (id) => {
 		const todos = this.state.todos.filter((todo) => {
 			return todo.id != id;
 		});
@@ -17,11 +18,17 @@ class App extends Component {
 			todos,
 		});
 	};
+	addTodo = (todo) => {
+		todo.id = Math.random();
+		let todos = [...this.state.todos, todo];
+		this.setState({ todos: todos });
+	};
 	render() {
 		return (
 			<div className='todo-app container'>
 				<h1 className='center blue-text'>Todo's</h1>
-				<Todos todos={this.state.todos} deleteTodo={this.delteteTodo} />
+				<Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+				<AddTodo addTodo={this.addTodo} />
 			</div>
 		);
 	}
