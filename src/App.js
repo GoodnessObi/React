def1da729ddc72d28847779c9b243a26e576b1b2
GoodnessObi <goodnessobi@gmail.com>
2020-08-28 20:43:48 +0100

    set up todo app, add delete event listener ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React, { Component } from 'react';
+import Todos from './Todos';
 
 class App extends Component {
+	state = {
+		todos: [
+			{ id: 1, content: 'buy some milk' },
+			{ id: 2, content: 'walk dog' },
+		],
+	};
+	delteteTodo = (id) => {
+		const todos = this.state.todos.filter((todo) => {
+			return todo.id != id;
+		});
+		this.setState({
+			// todos: todos,
+			todos,
+		});
+	};
 	render() {
-		return <div>Hiiiiii</div>;
+		return (
+			<div className='todo-app container'>
+				<h1 className='center blue-text'>Todo's</h1>
+				<Todos todos={this.state.todos} deleteTodo={this.delteteTodo} />
+			</div>
+		);
 	}
 }
 
